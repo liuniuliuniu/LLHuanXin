b1a8144a3151a8045ff1358d5a775946e152deaa
liushaohua <18703867769@163.com>
2017-01-08 17:16:35 +0800

    getMessage

---
+++ LLHuanXin/LLHuanXin/Controller/ChatDetail/LLChatDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  LLChatDetailViewController.h
+//  LLHuanXin
+//
+//  Created by liushaohua on 2017/1/8.
+//  Copyright © 2017年 liushaohua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LLChatDetailViewController : UITableViewController
+
+@end
